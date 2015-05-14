cd7b0606eb40c9652611f78226d0f471d6853c00
@rgem52 <rgomez@hi-iberia.es>
2015-05-14 12:29:15 +0200

    Broadcast a message to connected users when someone connects

--- chat/index.js
+++ chat/index.js
@@ -13,7 +13,16 @@ io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
+
+    //Broadcast a message to connected users when someone connects
+    socket.broadcast.emit('chat message','New User Connected');
+
+    //Broadcast a message to connected users when someone disconnects
+    socket.on('disconnect', function(){
+        socket.broadcast.emit('chat message','User Disconnected');
+    });
 });
+
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
